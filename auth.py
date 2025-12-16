42ded92f27786c17dcf25e7f1504019fab9850df
Mangal9101 <mkp94065@gmail.com>
2025-12-16 22:02:31 +0530

    Fix bcrypt safe, add env secret

--- auth.py
+++ auth.py
@@ -1,12 +1,17 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
+import os
 
-SECRET_KEY = "SECRET123"
+SECRET_KEY = os.getenv("SECRET_KEY", "SECRET123")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+
+
 
 
 def hash_password(password: str):
